f87bd42b12d1be194f90c51f513379db245616c2
Iggy <igoririanto@rocketmail.com>
2016-01-29 17:03:01 -0800

    Assignment 26 done

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
+#Describes the subject of spec
 RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
-# #2
+# #2 - call the index METHOD of WelcomeController
       get :index
-# #3
+# #3 - expects the controller's response to render INDEX template.
+#2 and 3 are part of one test!
       expect(response).to render_template("index")
     end
   end
@@ -15,5 +17,13 @@ RSpec.describe WelcomeController, type: :controller do
       get :about
       expect(response).to render_template("about")
     end
-  end  
+  end
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
+
 end
