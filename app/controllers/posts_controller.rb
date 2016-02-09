23715901ea967b075e8cdda67216f6b6a3de24fe
Iggy <igoririanto@rocketmail.com>
2016-02-08 19:54:51 -0500

    Assignment 31: added controller code and removed view code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,15 @@ class PostsController < ApplicationController
   def index
 
     @posts = Post.all
+
+    @posts.each_with_index do |post, index| #making every 5 posts "SPAM"
+      if (index + 1) % 5 == 0
+        post.title = "SPAM"
+      else
+        post.title
+      end
+    end
+
   end
 
   def show
