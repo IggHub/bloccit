f0d9d9239e01f54f20a7a990158f7297a42f3690
Iggy <igoririanto@rocketmail.com>
2016-02-27 12:34:43 -0800

    Assignment 35 cmopleted

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,9 @@ require 'random_data'
 
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+#  validates :posts, presence: true
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15}, presence: true
+
 end
