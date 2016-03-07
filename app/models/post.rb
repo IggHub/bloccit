599a83cc8ed836992a05870d6ad2ac4a25c800e9
Iggy <igoririanto@rocketmail.com>
2016-03-06 23:22:19 -0800

    Assignment 39 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC')}
+  scope :ordered_by_title, -> {order('title DESC')}
+  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
+#first set the default_scope, describe what it will do
+#then list all the available scopes that this project will have by scope title, then describe how it will be ordered!
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20}, presence: true
